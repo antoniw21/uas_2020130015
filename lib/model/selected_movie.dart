7438afa416121b377ba39471c7be296c9405b739
Antoni Wijaya <ton302107@gmail.com>
2022-12-15 21:14:31 +0700

    model for get and set data

---
+++ lib/model/selected_movie.dart
@@ -0,0 +1,20 @@
+class SelectedMovie {
+  int index = 0;
+  bool boxoffice = false;
+
+  int get getIndex {
+    return index;
+  }
+
+  set setIndex(int a) {
+    index = a;
+  }
+
+  bool get getBoxoffice {
+    return boxoffice;
+  }
+
+  set setBoxoffice(bool e) {
+    boxoffice = e;
+  }
+}
